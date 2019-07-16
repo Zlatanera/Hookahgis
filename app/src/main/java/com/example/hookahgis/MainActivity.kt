9aa70fdd19215be45b75b587181354159f9ff11e
badjokecat <anotheracc4gp@gmail.com>
2019-07-16 23:59:48 +0700

    Добавлен логин и переход между окнами

--- app/src/main/java/com/example/hookahgis/MainActivity.kt
+++ app/src/main/java/com/example/hookahgis/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.hookahgis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun Age_yes(view: View){
+        val IntentOrder = Intent(applicationContext, Login::class.java)
+        startActivity(IntentOrder)
+        /*
+        IntentOrder - локальная переменная, так что в данные две строки можно использовать
+        как образец для перехода на другое активити, меняя "Login::class.java"
+        */
+    }
+
+    fun Age_no(view: View){
+        Toast.makeText(this, "Вы должны быть старше 18-ти!", Toast.LENGTH_LONG
+        ).show()
     }
 }
