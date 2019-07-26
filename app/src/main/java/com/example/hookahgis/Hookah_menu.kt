9f141c71cc125d8284483b89d4fc7c3dec2c1069
Zlatanera <borozdin54@gmail.com>
2019-07-27 00:29:52 +0700

    Начало работы над добавлением самих кальянов во вкладку

--- app/src/main/java/com/example/hookahgis/Hookah_menu.kt
+++ app/src/main/java/com/example/hookahgis/Hookah_menu.kt
@@ -1,12 +1,19 @@
 package com.example.hookahgis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Hookah_menu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hookah_menu)
+    }
+    fun Kalyany(view: View){
+        val IntentOrder = Intent(applicationContext, Kalyany::class.java)
+        startActivity(IntentOrder)
+
     }
 }
