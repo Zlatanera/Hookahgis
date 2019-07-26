be4838b085a4aaacf6096b5846e2fed50d4f48b7
badjokecat <anotheracc4gp@gmail.com>
2019-07-27 00:34:40 +0700

    Начата работа над кальянными

--- app/src/main/java/com/example/hookahgis/Hookah_menu.kt
+++ app/src/main/java/com/example/hookahgis/Hookah_menu.kt
@@ -1,12 +1,19 @@
 package com.example.hookahgis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Hookah_menu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hookah_menu)
+    }
+
+    fun where_to_smoke(view: View){
+        val hookah_place = Intent(this, gde_kurim::class.java)
+        startActivity(hookah_place)
     }
 }
---
+++ app/src/main/java/com/example/hookahgis/gde_kurim.kt
@@ -0,0 +1,12 @@
+package com.example.hookahgis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class gde_kurim : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gde_kurim)
+    }
+}
